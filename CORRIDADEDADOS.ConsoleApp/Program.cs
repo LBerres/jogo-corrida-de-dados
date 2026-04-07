ecb4ed62ba517eb17053425ef5eacbc4eff171a2
Leandro Berres Pereira <leandro.motorhead@gmail.com>
2026-04-07 20:01:52 -0300

    Adicionado Projeto Inicial

---
+++ CORRIDADEDADOS.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Especificação do Jogo de Corrida de Dados
+Objetivo do Jogo: O jogador e o computador competem em uma corrida virtual. Cada um rola um dado virtua
+(geração de números aleatórios) para avançar em uma pista. O primeiro a alcançar ou ultrapassar a linha d
+chegada vence.
+
+Regras e Funcionalidades:
+1. Pista:
+○ A pista é representada por uma linha numérica (ex.: de 0 a 30).
+○ O jogador e o computador começam na posição 0.
+
+*/
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("JOGO DE CORRIDA DE DADOS");
+Console.WriteLine("-------------------------------------------");
+
+
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Pressione Enter para sair...");
+Console.ReadLine();
